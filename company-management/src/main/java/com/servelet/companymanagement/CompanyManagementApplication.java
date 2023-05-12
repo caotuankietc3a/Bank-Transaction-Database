2783ca8773673d60de628feefd73415026e789fe
caotuankietc3a <caotuankietc3a@gmail.com>
2023-05-12 21:03:23 +0700

    feat: Starting Company management

---
+++ company-management/src/main/java/com/servelet/companymanagement/CompanyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.servelet.companymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManagementApplication.class, args);
+	}
+
+}
---
+++ company-management/src/test/java/com/servelet/companymanagement/CompanyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servelet.companymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
